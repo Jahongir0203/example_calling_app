a894ba72bd711761d88684ea8e581e8a5b028f32
Jahongir0203 <you@example.com>
2023-02-16 14:22:29 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:math' as math;
+import 'utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+final String userlocalID = math.Random().nextInt(1000).toString();
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  final callIDTextCtrl = TextEditingController(text: 'Write calling ID ....');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Expanded(child:
+            TextFormField(
+              controller: callIDTextCtrl,
+              decoration: InputDecoration(
+                  labelText: 'Join a call by ID '
+              ),
+            )
+            ),
+              ElevatedButton(onPressed:(){Navigator.push(
+                  context, MaterialPageRoute(builder: (context) {
+                return CallPage(
+                    callID:callIDTextCtrl.text
+                );
+              }
+              )
+              );
+              }, child: Text('Join'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CallPage extends StatelessWidget {
+
+  final String callID;
+  CallPage({required this.callID});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child:
+    ZegoUIKitPrebuiltCall(
+      appID:Utils.appId,
+      appSign: Utils.appSignin,
+      userID:userlocalID ,
+      userName:"User_$userlocalID" ,
+      callID:callID ,
+      config:ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall() ..onOnlySelfInRoom=(context){
+        Navigator.of(context).pop();
+      },
+
+
+    ));
+  }
+}
+
---
+++ lib/utils.dart
@@ -0,0 +1,6 @@
+
+class Utils{
+  static int appId=469548375;
+  static String appSignin="7fb6f161c237fc26cd51ec1279189c4abc7c0f1b8d67884c0f7fe80b09252ffe";
+
+}
